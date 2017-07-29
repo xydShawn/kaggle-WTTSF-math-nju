b2d4bc97742aa034c83455c27b403184c6ab66fa
MengyaoLyu <alremy@outlook.com>
2017-07-29 10:11:33 +0800

    clone from last version, delete some files, add model_py and make first submission using multi task lasso

--- utils.py
+++ utils.py
@@ -10,6 +10,34 @@ def strc(x):
     s = str(x) if x >= 10 else '0' + str(x)
     return s
 
+def generate_submission_2(result):
+    ttime = datetime.now()
+    time2str = strc(ttime.month) + strc(ttime.day) + strc(ttime.hour) + strc(ttime.minute)
+    filename = '../result/submission_' + time2str + '.csv'
+    days = 60
+    page_num = 145063
+    if isinstance(result, pd.DataFrame):
+        temp = result.values
+    elif isinstance(result, np.ndarray):
+        temp = result
+    else:
+        print('to do')
+    submission_result = pd.read_csv('../data/sample_submission_1.csv')
+    sub_res = submission_result.values
+    map_pos = pd.read_csv('../tmp/map_pos.csv').values
+
+
+    if temp.shape != (page_num, days):
+        print('to do')
+        break
+
+    with open(filename, 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Id', 'Visits'])
+        for i in range(page_num * days):
+            value = result[map_pos[i, 0], map_pos[i, 1]]
+            writer.writerow([sub_res[i, 0], value])
+
 def generate_submission_1(result):
     submission_result = pd.read_csv('./data/sample_submission_1.csv')
     map_pos = pd.read_csv('./tmp/map_pos.csv').values
@@ -18,7 +46,7 @@ def generate_submission_1(result):
 
     if isinstance(result, pd.DataFrame):
         temp = result.values
-    elif isinstance(result, np.array):
+    elif isinstance(result, np.ndarray):
         temp = result
     else:
         print('to do')
@@ -28,7 +56,7 @@ def generate_submission_1(result):
             submission_result.loc[i, 'Visits'] = temp[map_pos[i, 0], map_pos[i, 1]]
         ttime = datetime.now()
         time2str = strc(ttime.month) + strc(ttime.day) + strc(ttime.hour) + strc(ttime.minute)
-        filename = './result/submission_' + time2str + '.csv'
+        filename = '../result/submission_' + time2str + '.csv'
         submission_result.to_csv(filename, index=False)
     else:
         print('to do')
