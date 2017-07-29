617cb8e862107553d240596a6f2c2ce21c04c572
xydShawn <jsntlbxyd@gmail.com>
2017-07-29 21:08:39 +0800

    提交关于统计量的模型

---
+++ model_py/stat_quantity.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+from utils import *
+
+train_data = pd.read_csv('../data/train_1.csv')
+print('fetch data')
+
+num_ttw = 56
+stat = 'median'
+
+sub_columns = train_data.columns[-num_ttw:]
+sub_data = train_data[sub_columns]
+sub_data_pre = sub_data.fillna(0)
+print('preprocessing done')
+
+result = np.zeros((len(train_data), 60))
+
+if stat == 'median':
+    xx = sub_data_pre.median(axis=1)
+elif stat == 'mean':
+    xx = sub_data_pre.mean(axis=1)
+else:
+    print('to do')
+
+for i in range(60):
+    result[:, i] = xx
+print('completing')
+print('shape of result', result.shape)
+
+generate_submission_1(result)
